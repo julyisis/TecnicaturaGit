af535625eb0ab5b3d7558bf2d3f283699ee5bd1f
JULIETA GUTIERREZ <julimaxizoe@gmail.com>
2022-11-05 21:29:50 -0300

    commit de Java leccion 5 encapsulamiento

---
+++ Java/LECCION5/Encapsulamiento/src/test/PersonaPrueba.java
@@ -0,0 +1,13 @@
+/*
+
+*/
+package test;
+
+import dominio.Persona;
+public class PersonaPrueba {
+    public static void main(String[] args) {
+        Persona persona1 = new   Persona("Osvaldo",57.000,false);
+        //MODIFICAR A TRAVEZ DE LOS METODOS 
+    }
+    
+}
